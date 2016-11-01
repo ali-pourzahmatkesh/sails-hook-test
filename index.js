14d63bd3124b7f57b8c4c5902cdc8d983a906d9a
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2016-11-02 01:01:13 +0330

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = function(sails) {
+
+    return {
+        defaults: {
+        },
+        initialize: function(cb) {
+
+            var ModelA = require('./models/ModelA');
+            ModelA.identity = 'modela';
+            ModelA.globalid = 'ModelA';
+
+            var ModelB = require('./models/ModelB');
+            ModelB.identity = 'modelb';
+            ModelB.globalid = 'ModelB';
+
+            sails.models['modela'] = ModelA;
+            sails.models['modelb'] = ModelB;
+
+            return cb();
+        }
+    }

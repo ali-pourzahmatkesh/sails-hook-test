aa16d3bf40a7a02195cc5e25273f2cca2e8711df
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2016-11-02 02:22:24 +0330

    add /test route

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ module.exports = function(sails) {
 
     return {
         defaults: {},
+        configure: function(){
+/*            sails.config.policies = {
+                '*': false
+            };*/
+        },
+        routes: {
+            before: {
+                'GET /test': 'TestController.test'
+            }
+        },
         initialize: function (cb) {
 
             var TestModel = require('./models/Test');

f8de28c3b562e316469d92f89a093ae162cff0ed
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2016-11-02 23:02:13 +0330

    add routes

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ module.exports = function(sails) {
                 '*': false
             };*/
         },
-        routes: {
+/*        routes: {
             before: {
                 'GET /test': 'TestController.test'
             }
-        },
+        },*/
         initialize: function (cb) {
+
+            sails.on('router:before', function () {
+
+                sails.router.bind('GET /test', 'TestController.test');
+
+            });
 
             var TestModel = require('./models/Test');
             TestModel.identity = 'test';

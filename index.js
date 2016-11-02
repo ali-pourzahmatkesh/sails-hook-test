4de32085e17a96e238c123cb1cd841a8cfdce1ed
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2016-11-03 01:22:07 +0330

    fix all bugs

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-module.exports = function(sails) {
-    var loader = require('sails-util-mvcsloader')(sails);
+module.exports = function (sails) {
 
-    // Load policies under ./api/policies and config under ./config
-    loader.configure();
+    console.log('start loading hook test...');
 
-    /*
-     OR if you want to set a custom path :
-
-     loader.configure({
-     policies: __dirname + '/api/policies',// Path to your hook's policies
-     config: __dirname + '/config'// Path to your hook's config
-     });
-     */
+    //Override http config here before hook initialisation
+    var hookLoader = require('sails-util-mvcsloader')(sails);
+    hookLoader.injectAll({
+        policies: __dirname + '/api/policies',// Path to your hook's policies
+        config: __dirname + "/config"// Path to your hook's config
+    });
 
     return {
-        initialize: function (next) {
-            /*
-             Load models under ./api/models
-             Load controllers under ./api/controllers
-             Load services under ./api/services
-             */
-            loader.inject(function (err) {
-                return next(err);
+        defaults: {},
+
+        initialize: function (cb) {
+
+            console.log('start initialize hook test...');
+            
+            hookLoader.injectAll({
+                controllers: __dirname + '/api/controllers', // Path to your hook's controllers
+                models: __dirname + '/api/models', // Path to your hook's models
+                services: __dirname + '/api/services' // Path to your hook's services
+            }, function (err) {
+                if (!err) {
+                    console.log('end hook test...');
+                }
+                return cb(err);
             });
 
-            /*
-             OR if you want to set a custom path :
-
-             loader.inject({
-             controllers: __dirname + '/controllers', // Path to your hook's controllers
-             models: __dirname + '/models', // Path to your hook's models
-             services: __dirname + '/services' // Path to your hook's services
-             }, function (err) {
-             return next(err);
-             });
-             */
         }
     };
-}
+};
